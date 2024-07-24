0cfa2c204b01d30738e5e8ec0ead1b02b3e2a67c
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2024-07-24 15:11:07 +0300

    Atakan Demir

--- js/distance_indicator.js
+++ js/distance_indicator.js
@@ -4,13 +4,13 @@ export function updateDistanceIndicator(){
     const bar = document.querySelector('.bar');
     const indicator = document.querySelector('.indicator');
     const barContainer = document.querySelector('.bar-container');
-let dist=0;
+    let dist=0;
 
-    function updateGauge(distance) {
-        if (distance < 0) distance = 0;
-        if (distance > totalDistance) distance = totalDistance;
+    function updateGauge(x) {
+        if (x < 0) x = 0;
+        if (x > totalDistance) x = totalDistance;
 
-        const percentage = (distance / totalDistance) * 100;
+        const percentage = (x / totalDistance) * 100;
         bar.style.width = percentage + '%';
         indicator.style.left = 'calc(' + percentage + '% - 15px)'; // 15px to center the object image
     }
